48f04869bd0a8bcedd5c5b2bb632c0a210a3f67f
thesnapdragon <u.milan@gmail.com>
2014-02-17 20:29:40 +0100

    game info moved to right on viewpage (closes #11)

--- template/view.html.go
+++ template/view.html.go
@@ -8,56 +8,65 @@
 <body>
     <div id="header_decorator"></div>
 
-    <div id="game-info">
-        {{if hasWinner .Winner}}
-            <a href="/games">Vissza az archívumhoz</a> <br/>
-        {{end}}
-        <div id="player-list">
-            <p class="title">Játékosok:</p>
-            <ul>
-                {{with .Players}}
-                    {{range .}}
-                        <li>
-                            {{.Name}}
-                            (talált: <span class="player-color-info" style="background-color: {{.Color.Hit}};"></span>,
-                            talált &amp; süllyedt: <span class="player-color-info" style="background-color: {{.Color.HitAndSunk}};"></span>)
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8">
+                    <table id="board">
+                        {{with .Board}}
+                            {{letters (len .Fields) "td"}}
 
-                        </li>
-                    {{end}}
-                {{end}}
-            </ul>
-            <div class="clearfix"></div>
-        </div>
+                            {{range $col, $ := .Fields}}
+                                <tr>
+                                    <td>{{add $col 1}}</td>
+                                    {{range $row, $ := .}}
+                                        <td class="field coord-{{$col}}-{{$row}}" style="background-color: {{ship_color .}};"></td>
+                                    {{end}}
+                                </tr>
+                            {{end}}
+                        {{end}}
+                    </table>
+            </div>
+            <div class="col-md-4">
+                    <div id="game-info">
+                        {{if hasWinner .Winner}}
+                            <a href="/games">Vissza az archívumhoz</a> <br/>
+                        {{end}}
+                        <div id="player-list">
+                            <p class="title">Játékosok:</p>
+                            <ul>
+                                {{with .Players}}
+                                    {{range .}}
+                                        <li>
+                                            {{.Name}}
+                                            (talált: <span class="player-color-info" style="background-color: {{.Color.Hit}};"></span>,
+                                            talált &amp; süllyedt: <span class="player-color-info" style="background-color: {{.Color.HitAndSunk}};"></span>)
 
-        <div class="winner hidden">
-            A nyertes: <span>[name]</span>
-        </div>
+                                        </li>
+                                    {{end}}
+                                {{end}}
+                            </ul>
+                            <div class="clearfix"></div>
+                        </div>
 
-        <div id="time">
-            A körből hátralevő idő: <span id="elapsed-time">0</span> s
-        </div>
+                        <div class="winner hidden">
+                            A nyertes: <span>[name]</span>
+                        </div>
 
-        <div id="players">
-            <p>Jelenlegi játékos: <span id="current-player"></span></p>
-            <p>Következő játékos: <span id="next-player"></span></p>
-            <div class="clearfix"></div>
+                        <div id="time">
+                            A körből hátralevő idő: <span id="elapsed-time">0</span> s
+                        </div>
+
+                        <div id="players">
+                            <p>Jelenlegi játékos: <span id="current-player"></span></p>
+                            <p>Következő játékos: <span id="next-player"></span></p>
+                            <div class="clearfix"></div>
+                        </div>
+                    </div>
+            </div>
         </div>
     </div>
 
-    <table id="board">
-        {{with .Board}}
-            {{letters (len .Fields) "td"}}
 
-            {{range $col, $ := .Fields}}
-                <tr>
-                    <td>{{add $col 1}}</td>
-                    {{range $row, $ := .}}
-                        <td class="field coord-{{$col}}-{{$row}}" style="background-color: {{ship_color .}};"></td>
-                    {{end}}
-                </tr>
-            {{end}}
-        {{end}}
-    </table>
 
     <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
     <script type="text/javascript" src="/public/js/board.js"></script>
